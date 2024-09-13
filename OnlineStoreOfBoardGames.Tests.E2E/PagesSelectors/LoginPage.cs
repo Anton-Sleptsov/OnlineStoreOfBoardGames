ef4c3e90e0b08d622de0fff08069cf556027de56
Anton-Sleptsov <148457038+Anton-Sleptsov@users.noreply.github.com>
2024-09-13 16:41:43 +0300

    Added compose

---
+++ OnlineStoreOfBoardGames.Tests.E2E/PagesSelectors/LoginPage.cs
@@ -0,0 +1,11 @@
+﻿using OpenQA.Selenium;
+
+namespace OnlineStoreOfBoardGames.Tests.E2E.PagesSelectors
+{
+    public static class LoginPage
+    {
+        public static By LoginInput = By.CssSelector("#Login");
+        public static By PasswordInput = By.CssSelector("#Password");
+        public static By SubmitButton = By.CssSelector("[type=submit]");
+    }
+}
