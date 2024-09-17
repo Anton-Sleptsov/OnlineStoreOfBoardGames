12391dcf13a40f4cd122cd5975ca3d23f0ea2626
Anton-Sleptsov <148457038+Anton-Sleptsov@users.noreply.github.com>
2024-09-17 23:57:48 +0300

    Added permissions for alerts

--- OnlineStoreOfBoardGames/Controllers/AlertController.cs
+++ OnlineStoreOfBoardGames/Controllers/AlertController.cs
@@ -1,29 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
+using OnlineStoreOfBoardGames.Controllers.ActionFilterAttributes;
 using OnlineStoreOfBoardGames.Data.Repositories.Interfaces;
 using OnlineStoreOfBoardGames.Hubs;
 using OnlineStoreOfBoardGames.Mappers;
 using OnlineStoreOfBoardGames.Models.Alert;
 using OnlineStoreOfBoardGames.Services;
+using OnlineStoreOfBoardGames.Data.Enums;
+using Microsoft.AspNetCore.Authorization;
+using OnlineStoreOfBoardGames.Services.AuthStuff.Interfaces;
 
 namespace OnlineStoreOfBoardGames.Controllers
 {
+    [Authorize]
     public class AlertController : Controller
     {
-        private IAlertRepository _alertRepository;
-        private AlertMapper _alertMapper;
+        private readonly IAlertRepository _alertRepository;
+        private readonly AlertMapper _alertMapper;
         public IHubContext<AlertHub, IAlertHub> _alertHub;
         public LocalizatoinService _localizatoinService;
+        private readonly IAuthService _authServise;
 
         public AlertController(IAlertRepository alertRepository,
             AlertMapper alertMapper,
             IHubContext<AlertHub, IAlertHub> alertHub,
-            LocalizatoinService localizatoinService)
+            LocalizatoinService localizatoinService,
+            IAuthService authServise)
         {
             _alertRepository = alertRepository;
             _alertMapper = alertMapper;
             _alertHub = alertHub;
             _localizatoinService = localizatoinService;
+            _authServise = authServise;
         }
 
         public IActionResult Index()
@@ -41,15 +49,23 @@ namespace OnlineStoreOfBoardGames.Controllers
                 alertViewModels.Add(alertViewModel);
             }
 
-            return View(alertViewModels);
+            var alertsViewModel = new AlertsIndexViewModel
+            {
+                CanCreateAndDelete = _authServise.HasPermission(Permission.CanCreateAndDeleteAlerts),
+                Alerts = alertViewModels
+            };
+
+            return View(alertsViewModel);
         }
 
+        [HasPermission(Permission.CanCreateAndDeleteAlerts)]
         [HttpGet]
         public IActionResult Create()
         {
             return View();
         }
 
+        [HasPermission(Permission.CanCreateAndDeleteAlerts)]
         [HttpPost]
         public async Task<IActionResult> Create(AlertCreateViewModel viewModel)
         {
@@ -59,6 +75,11 @@ namespace OnlineStoreOfBoardGames.Controllers
             }
 
             var alert = _alertMapper.MapToDataBaseModel(viewModel);
+
+            if(alert.EndDate is not null)
+            {
+                alert.EndDate = alert.EndDate.Value.ToUniversalTime();
+            }
             _alertRepository.Create(alert);
 
             await _alertHub.Clients.All.AlertWasCreatedAsync(alert.Id, alert.Text);
@@ -66,6 +87,7 @@ namespace OnlineStoreOfBoardGames.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        [HasPermission(Permission.CanCreateAndDeleteAlerts)]
         public IActionResult Delete(int id)
         {
             _alertRepository.Delete(id);
